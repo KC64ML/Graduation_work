56360b07fc79e2c1924c498cfdef8bf12d3967ef
chae1405 <chae1405@nate.com>
2021-05-18 10:06:28 +0900

    text example

--- graduation_work/app/src/main/java/com/example/image11/MainActivity.kt
+++ graduation_work/app/src/main/java/com/example/image11/MainActivity.kt
@@ -17,51 +17,10 @@ import com.example.image11.R
 class MainActivity : AppCompatActivity() {
 
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val imbt1 = findViewById<ImageButton>(R.id.imageButton);
-        val imbt2 = findViewById<ImageButton>(R.id.imageButton2);
-        val imbt3 = findViewById<ImageButton>(R.id.imageButton3);
-        val imbt4 = findViewById<ImageButton>(R.id.imageButton4);
-
-
-        imbt1.setOnClickListener{
-                val intent = Intent(this, SubActivity4::class.java)//다음화면으로 이동하기위한 인텐트 객체생성
-
-                startActivity(intent);
-
-            }
-
-            imbt2 .setOnClickListener{
-
-                val intent = Intent(this, SubActivity1::class.java)//다음화면으로 이동하기위한 인텐트 객체생성
-
-                startActivity(intent);
-
-            }
-
-            imbt3 .setOnClickListener{
-
-                val intent = Intent(this, SubActivity2::class.java)//다음화면으로 이동하기위한 인텐트 객체생성
-
-                startActivity(intent);
-
-            }
-            imbt4.setOnClickListener{
-
-                val intent = Intent(this, SubActivity3::class.java)//다음화면으로 이동하기위한 인텐트 객체생성
-
-                startActivity(intent);
-
-            }
-
-
-
-
-
-
-
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
         }
     }
+}
