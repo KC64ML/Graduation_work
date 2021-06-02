801e04de76af6470810d954b2c84b784b7fec113
unknown <lkc263@naver.com>
2021-06-02 16:05:51 +0900

    logg

--- GraduationWorkOk/app/src/main/java/image11/graduation/ok/com/Regist.kt
+++ GraduationWorkOk/app/src/main/java/image11/graduation/ok/com/Regist.kt
@@ -2,10 +2,17 @@ package image11.graduation.ok.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Regist : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.check)
+
+
+        val imot1 = findViewById<ImageButton>(R.id.copy);
+        val imot2 = findViewById<ImageButton>(R.id.imageButton26);
+
+
     }
 }
